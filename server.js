07ac079173902518702f1003fb39e436891291a7
vivifrasa <vivifrasa@gmail.com>
2020-12-12 23:51:03 -0500

    Revert "arreglo cors"his reverts commit 6b2764db1063fbbeaa50d94439cfd795e31e1f6a.
    This reverts commit 6b2764db1063fbbeaa50d94439cfd795e31e1f6a.

--- server.js
+++ server.js
@@ -3,9 +3,6 @@ const serveStatic = require('serve-static')
 const path = require('path')
 const app = express()
 //here we are configuring dist to serve app files
-app.use(cors());
-app.options('*', cors()); // this will enable cors for all your route.
-
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
 // this * route is to serve project on different page routes except root `/`
 app.get(/.*/, function (req, res) {
@@ -14,8 +11,4 @@ res.sendFile(path.join(__dirname, '/dist/index.html'))
 const port = process.env.PORT || 8080
 app.listen(port)
 console.log(`app is listening on port: ${port}`)
-
-app.use(cors());
-
-
 
